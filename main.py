01bb2588e59e8307499b3678086b519ab0129165
Nikita Belyak <nikita.belyak1994@gmail.com>
2024-06-19 12:46:57 +0300

    add describtion

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from parameters import WindFarmParameters
 from energy_efficiency_function import calculate_energy_efficiency
 
-wind_farm = WindFarmParameters(16,153)
+n_turbines = 16  # number of turbines
+random_seed = 153 # random seed for reproducibility
+
+# generate wind farm 
+wind_farm = WindFarmParameters(n_turbines, random_seed)
+
+# calculate energy efficiency
 calculate_energy_efficiency(wind_farm)
 
 
